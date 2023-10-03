9f1eed588845604ddf06cc975e6e82fb7980a818
Eunji (Silvia) Kwon <sandeul97@gmail.com>
2023-10-03 01:11:57 -0400

    [Bronze V] Title: 사칙연산, Time: 64 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,6 @@
+string[] a = Console.ReadLine().Split();
+Console.WriteLine(int.Parse(a[0])+int.Parse(a[1]));
+Console.WriteLine(int.Parse(a[0])-int.Parse(a[1]));
+Console.WriteLine(int.Parse(a[0])*int.Parse(a[1]));
+Console.WriteLine(int.Parse(a[0])/int.Parse(a[1]));
+Console.WriteLine(int.Parse(a[0])%int.Parse(a[1]));
