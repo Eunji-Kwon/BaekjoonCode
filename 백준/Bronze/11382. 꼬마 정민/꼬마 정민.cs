cd2ac7b409d62d34f53955b31c94e65ed37cfe91
Eunji (Silvia) Kwon <sandeul97@gmail.com>
2023-10-09 14:40:24 -0400

    [Bronze V] Title: 꼬마 정민, Time: 64 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -0,0 +1,4 @@
+ string[] num = Console.ReadLine().Split();
+            Console.WriteLine(long.Parse(num[0])
+                       + long.Parse(num[1])
+                       + long.Parse(num[2]));
